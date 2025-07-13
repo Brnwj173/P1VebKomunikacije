0e446ae1199ad898a063b0ea2a49ae688d09dfc5
Brnwj173 <155011416+Brnwj173@users.noreply.github.com>
2025-07-13 20:53:59 +0200

    Add files via upload

---
+++ Gaming Lounge/js/script.js
@@ -0,0 +1,4 @@
+// Function to show a promotional message when the button is clicked  
+function showPromotion() {  
+  alert("🔥 Book now and get 1 hour free with every 3 hours purchased! 🔥");  
+}  
