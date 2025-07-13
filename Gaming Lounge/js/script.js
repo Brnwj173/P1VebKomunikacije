975b4df3faf6488a1a1c5dbb057199c1393202c2
Brnwj173 <155011416+Brnwj173@users.noreply.github.com>
2025-07-14 01:24:51 +0200

    Update script.js

--- Gaming Lounge/js/script.js
+++ Gaming Lounge/js/script.js
@@ -1,4 +1,3 @@
-// Function to show a promotional message when the button is clicked  
 function showPromotion() {  
   alert("🔥 Rezerviši sada i dobijaš 1 sat besplatno za svaka 3 sata igranja! 🔥");  
-}  
+}
